818bff4a46ca267ce490027315f23bde2dd6d6db
kevins981 <kevinsong981@gmail.com>
2024-09-29 13:01:23 -0400

    Add GAPBS page rank workload to our setup

--- memtis-userspace/bench_cmds/gapbs-pr.sh
+++ memtis-userspace/bench_cmds/gapbs-pr.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 ######## changes the below path
-BIN=/path/to/benchmark
-GRAPH_DIR=/path/to/benchmark/benchmark/graphs
+BIN=/ssd1/songxin8/thesis/graph/gapbs_ecosys/pr
 
-BENCH_RUN="${BIN}/pr -f ${GRAPH_DIR}/twitter.sg -i1000 -t1e-4 -n20"
+BENCH_RUN="${BIN} -g 31 -k 4 -i1000 -t1e-4 -n16"
 BENCH_DRAM=""
 
+echo "bench run:"
+echo $BENCH_RUN
 
-# twitter.sg: ~12600MB 
 
 if [[ "x${NVM_RATIO}" == "x1:32" ]]; then
-    BENCH_DRAM="382MB"
+    BENCH_DRAM="16000MB"
 elif [[ "x${NVM_RATIO}" == "x1:16" ]]; then
-    BENCH_DRAM="740MB"
+    BENCH_DRAM="32000MB"
 elif [[ "x${NVM_RATIO}" == "x1:8" ]]; then
-    BENCH_DRAM="1400MB"
-elif [[ "x${NVM_RATIO}" == "x1:4" ]]; then
-    BENCH_DRAM="2520MB"
-elif [[ "x${NVM_RATIO}" == "x1:2" ]]; then
-    BENCH_DRAM="4200MB"
-elif [[ "x${NVM_RATIO}" == "x1:1" ]]; then
-    BENCH_DRAM="6300MB"
-elif [[ "x${NVM_RATIO}" == "x1:0" ]]; then
-    BENCH_DRAM="70000MB"
+    BENCH_DRAM="64000MB"
+#elif [[ "x${NVM_RATIO}" == "x1:4" ]]; then
+#    BENCH_DRAM="2520MB"
+#elif [[ "x${NVM_RATIO}" == "x1:2" ]]; then
+#    BENCH_DRAM="4200MB"
+#elif [[ "x${NVM_RATIO}" == "x1:1" ]]; then
+#    BENCH_DRAM="6300MB"
+#elif [[ "x${NVM_RATIO}" == "x1:0" ]]; then
+#    BENCH_DRAM="70000MB"
 fi
 
 
