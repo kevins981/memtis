162d63705c783f29578d52b620c6f710faeab795
kevins981 <kevinsong981@gmail.com>
2025-02-10 17:41:14 -0500

    Add urand input graph to GAP

--- memtis-userspace/run-gapbs.sh
+++ memtis-userspace/run-gapbs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-BENCHMARKS="gapbs-cc gapbs-bfs gapbs-pr"
-#NVM_RATIO="1:32 1:16 1:8"
-#BENCHMARKS="gapbs-cc gapbs-pr"
-NVM_RATIO="1:4"
+#BENCHMARKS="gapbs-cc gapbs-bfs gapbs-pr"
+BENCHMARKS="gapbs-cc-urand gapbs-bfs-urand gapbs-pr-urand"
+NVM_RATIO="1:16 1:8 1:4"
 
 NUM_THREADS=16
 export OMP_NUM_THREADS=${NUM_THREADS}
@@ -14,7 +13,8 @@ for BENCH in ${BENCHMARKS};
 do
     for NR in ${NVM_RATIO};
     do
-	  #./scripts/run_bench.sh -B ${BENCH} -R ${NR} --cxl --huge -V memtis-cxl-withhuge
 	  ./scripts/run_bench.sh -B ${BENCH} -R ${NR} --cxl -V memtis-cxl
+	  ./scripts/run_bench.sh -B ${BENCH} -R ${NR} --cxl --huge -V memtis-cxl-withhuge
     done
 done
+
